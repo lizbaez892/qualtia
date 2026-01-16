c8237f7cd12a774814ccc40336c946749c29223b
lizbaez892 <daniela.baez.2d@gmail.com>
2026-01-16 16:06:41 -0600

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,30 @@
+import '@/styles/globals.css';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
+import type { AppProps } from 'next/app';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider {...pageProps} appearance={{
+      cssLayerName: 'clerk'
+    }}>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+}
+
+export default MyApp;
