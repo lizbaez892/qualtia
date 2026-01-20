fb5fd5a1d6879b6ba30e2847f79e164b6c9a3c35
lizbaez892 <daniela.baez.2d@gmail.com>
2026-01-20 09:53:28 -0600

    middleware.ts

--- app/app/middleware.ts
+++ app/app/middleware.ts
@@ -1,1 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  const basicAuth = request.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    const USERNAME = 'qualtiamx'
+    const PASSWORD = 'qualtiamx'
+
+    if (user === USERNAME && pwd === PASSWORD) {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Acceso restringido', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
 
